c5bb980020cbe97093f1e51cf6623aaf70adcccb
Mazen Hmaidan <Mazen.Hmaidan@dwtc.com>
2025-04-02 15:08:09 +0400

    Allow CORS

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from openai import AzureOpenAI
 
 from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # or restrict to ["http://localhost:3000"] for security
@@ -13,8 +15,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-app = FastAPI()
 
 # Azure OpenAI client setup
 client = AzureOpenAI(
